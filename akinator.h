c388d81e8458e3875df65e928a6467fb1288c825
nerett <pt-mvb@inbox.lv>
2022-04-09 13:55:44 +0300

    added akinator.h and akinator.cpp

---
+++ akinator.h
@@ -0,0 +1,28 @@
+#ifndef AKINATOR_H_INCLUDED
+#define AKINATOR_H_INCLUDED
+
+
+#include "bintree.h"
+
+
+class Akinator
+{
+	private:
+
+		Tree akitree_;
+
+	public:
+
+	private:
+
+	public:
+
+		int play();
+
+		explicit Akinator();
+		~Akinator();
+
+};
+
+
+#endif //AKINATOR_H_INCLUDED
